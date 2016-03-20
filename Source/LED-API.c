9f66351e790f04c62e89c4af25cfccb384f3dd6e
Lars Gardien <larsgardien@live.nl>
2016-03-20 20:48:48 +0100

    [CORE] Reverted packed char, now using 2d array for individual pixels

--- Source/LED-API.c
+++ Source/LED-API.c
@@ -13,35 +13,47 @@
 // if bit on n# 'column' in LAT register is high, modify. else don't modify.
 #define setRowLow(row) (LATC = (LATC & (1 << row)) ? LATC ^ (1 << row) : LATC)
 
-#define setBit(x, n) (x[n/8] |= (1 << (n%8))
-#define clrBit(x, n) (x[n/8] &= ~(1 << (n%8)))
-#define getBit(x, n) ( (x[n/8] & (1 << (n%8) )) != 0)
+//#define setBit(x, n) (x[n/8] |= (1 << (n%8))
+//#define clrBit(x, n) (x[n/8] &= ~(1 << (n%8)))
+//#define getBit(x, n) ( (x[n/8] & (1 << (n%8) )) != 0)
+
+#define COLUMNS 8
+#define ROWS 16
 
 #include "LED-API.h"
+//
+//unsigned char columns[1];
+//unsigned char rows[1];
 
-unsigned char columns[1];
-unsigned char rows[1];
+unsigned char pixels[COLUMNS][ROWS];
 
 void initializeLED() {
-    TRISA = 0b00000000;
-    TRISC = 0b00000000;
-    LATC = 0b00000000;
-    LATA = 0b00000000;
+    TRISA = 0b00000000; // configure all pins as output
+    TRISC = 0b00000000; // configure all pins as outpur
+    LATC = 0b00000000; // write 0 to all pins
+    LATA = 0b00000000; // write 0 to all pins
 
-    //TODO: for loop when implementing 16 : 8
-    columns[0] = 0;
-    rows[0] = 0;
-    
+//    columns[0] = 0;
+//    rows[0] = 0;
+    for(unsigned short i; i < COLUMNS; i++)
+    {
+        for (unsigned short j; j < ROWS; j++)
+        {
+            pixels[i][j] = 0;
+        }
+    }
 }
 
 void on(unsigned char row, unsigned char column) {
-    if (row < 0 || row > 7)
+    if (row < 0 || row > ROWS)
         return;
-    if (column < 0 || column > 7)
+    if (column < 0 || column > COLUMNS)
         return;
 
-    setBit(pixels[0], column);
-    setBit(rows, row);
+//    setBit(columns, column);
+//    setBit(rows, row);
+    
+    pixels[column][row] = 1;
 }
 
 void off(unsigned char row, unsigned char column) {
@@ -50,21 +62,21 @@ void off(unsigned char row, unsigned char column) {
     if (column < 0 || column > 7)
         return;
 
-    clrBit(columns, column);
-    clrBit(rows, row);
+//    clrBit(columns, column);
+//    clrBit(rows, row);
+    pixels[column][row] = 0;
 }
 
 void refresh() {   
-    for(unsigned char theRow = 0; theRow < 8; theRow++)
+    for(unsigned char theRow = 0; theRow < ROWS; theRow++)
     {
         setRowHigh(theRow);
-        for (unsigned char theCol = 0; theCol < 8; theCol++) {
-            if (getBit(columns, theCol) && getBit(rows, theRow))
+        for (unsigned char theCol = 0; theCol < COLUMNS; theCol++) {
+            if (pixels[theCol][theRow] == 1)
                 setColHigh(theCol);
             else
                 setColLow(theCol);          
         }
         setRowLow(theRow);
-    }
-    
+    }  
 }
