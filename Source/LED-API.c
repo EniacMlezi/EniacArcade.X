a7bedb5e9e7e4652c95605353bc3dc188a7c0b82
Lars Gardien <larsgardien@live.nl>
2016-03-20 21:28:46 +0100

    [CORE] Did some small docu

--- Source/LED-API.c
+++ Source/LED-API.c
@@ -13,18 +13,12 @@
 // if bit on n# 'column' in LAT register is high, modify. else don't modify.
 #define setRowLow(row) (LATC = (LATC & (1 << row)) ? LATC ^ (1 << row) : LATC)
 
-//#define setBit(x, n) (x[n/8] |= (1 << (n%8))
-//#define clrBit(x, n) (x[n/8] &= ~(1 << (n%8)))
-//#define getBit(x, n) ( (x[n/8] & (1 << (n%8) )) != 0)
-
 #define COLUMNS 8
 #define ROWS 16
 
 #include "LED-API.h"
-//
-//unsigned char columns[1];
-//unsigned char rows[1];
 
+//array which will hold the columns and rows to pull high on next refresh
 unsigned char pixels[COLUMNS][ROWS];
 
 void initializeLED() {
@@ -33,8 +27,7 @@ void initializeLED() {
     LATC = 0b00000000; // write 0 to all pins
     LATA = 0b00000000; // write 0 to all pins
 
-//    columns[0] = 0;
-//    rows[0] = 0;
+    //write 0 to the pixels array
     for(unsigned short i; i < COLUMNS; i++)
     {
         for (unsigned short j; j < ROWS; j++)
@@ -45,25 +38,22 @@ void initializeLED() {
 }
 
 void on(unsigned char row, unsigned char column) {
+    //check out of bounds
     if (row < 0 || row > ROWS)
         return;
     if (column < 0 || column > COLUMNS)
         return;
-
-//    setBit(columns, column);
-//    setBit(rows, row);
     
     pixels[column][row] = 1;
 }
 
 void off(unsigned char row, unsigned char column) {
-    if (row < 0 || row > 7)
+    //check out of bounds
+    if (row < 0 || row > ROWS)
         return;
-    if (column < 0 || column > 7)
+    if (column < 0 || column > COLUMNS)
         return;
 
-//    clrBit(columns, column);
-//    clrBit(rows, row);
     pixels[column][row] = 0;
 }
 
@@ -75,7 +65,7 @@ void refresh() {
             if (pixels[theCol][theRow] == 1)
                 setColHigh(theCol);
             else
-                setColLow(theCol);          
+                setColLow(theCol);        
         }
         setRowLow(theRow);
     }  
