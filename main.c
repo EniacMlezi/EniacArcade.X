757d4eb45ed9a387bd6bfddec52ca8dcb0a69af7
Lars Gardien <larsgardien@live.nl>
2016-03-21 20:51:49 +0100

    [CORE] LED-interrupts first implementation {not working}

--- main.c
+++ main.c
@@ -10,6 +10,35 @@
 #include "Header/config.h"
 #include "Header/LED-API.h"
 
+
+void initialize()
+{ 
+    GIE = 1; // gloval interrupt enabled
+    PEIE=1; // peripheral interropt enabled
+    
+    //setup Timer0 for LED refresh
+    T0PS0 = 0; 
+    T0PS1 = 0;
+    T0PS2 = 0; // set prescaler to 1:2 for 500us timer when xtal is 1MHz
+    PSA = 0; // Timer0 prescaler enabled
+    T0CS = 0; // set Timer0 clock to internal instruction cycle clock
+    T08BIT = 1; // Timer0 is configured as an 8-bit timer/counter 
+    TMR0IE = 1; // set TIMER0 Interrupt Enable bit to enabled     
+    //start the timer
+    TMR0ON = 1; // set Timer0 On/Off Control bit to on  
+}
+
+//Interrupt Service Routine
+void interrupt ISR(void)
+{
+    if (TMR0IE && TMR0IF)
+    {
+        TMR0IF=0;
+        refresh();
+        TMR0ON = 1;
+    }
+}
+
 void delaySeconds(int numberOfSeconds)
 {
     for(int i = 0; i < numberOfSeconds * 100; i++)
@@ -18,11 +47,13 @@ void delaySeconds(int numberOfSeconds)
 
 void main(void) { 
     initializeLED();
-    
+    initialize();
     on(2, 2);
     
-    while(1)
-    {
-        refresh();
-    }
+    delaySeconds(3);
+    
+    off(2, 2);
+    on(3, 3);
+    
+    while(1);
 }
