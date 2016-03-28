9a56de3cfa5a32c6f80b5c772883a34d80aa6e88
nickd <nickd@DESKTOP-GP8THSR>
2016-03-28 17:37:46 +0200

    Added LED-API to commit

---
+++ Header/LED-API.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   LED-API.h
+ * Author: larsg
+ *
+ * Created on 20 maart 2016, 9:35
+ */
+
+#ifndef LED_API_H
+#define	LED_API_H
+
+#include <xc.h>
+
+extern void on(unsigned char column, unsigned char row);
+extern void rowOn(unsigned char row);
+extern void off(unsigned char column, unsigned char row);
+extern void rowOff(unsigned char row);
+extern void initializeLED();
+extern void refresh();
+
+#endif	/* LED_API_H */
+
